41f583e78ae0f8b1e20618248303389f8a92d62b
Elvis Pestana <elvispt@gmail.com>
2015-03-15 16:39:20 +0000

    Now saving news to browser localStorage. Data is saved as JSON and is the first data to load before attempting to connect to firebase to get the latest news. Now using lodash.js How long ago property has been removed and is now calculated on the view.

--- app/service/HackerNewsAPI.js
+++ app/service/HackerNewsAPI.js
@@ -1,41 +1,43 @@
-app.service("HackerNewsAPI", function () { 'use strict';
-    var CONFIG = {
-        max_num_stories: 100,
-        hn_type_story: "story",
-        hn_references: {
-            top_stories: "https://hacker-news.firebaseio.com/v0/topstories",
-            story: "https://hacker-news.firebaseio.com/v0/item/%s"
-        },
-        hn_story_url: "https://news.ycombinator.com/item?id=%s"
-    };
+app.service("HackerNewsAPI", function () {
+    'use strict';
 
-    // Attach an asynchronous callback to read the data at our posts reference
-    // This function will be called anytime new data is added to our Firebase reference, and we don"t need to write any extra code to make this happen.
-    var firebase = new Firebase(CONFIG.hn_references.top_stories).limitToFirst(CONFIG.max_num_stories);
+    var CONFIG = {
+            max_num_stories: 100,
+            hn_type_story: "story",
+            hn_references: {
+                top_stories: "https://hacker-news.firebaseio.com/v0/topstories",
+                story: "https://hacker-news.firebaseio.com/v0/item/%s"
+            },
+            hn_story_url: "https://news.ycombinator.com/item?id=%s"
+        },
+        firebase_endpoint = new Firebase(CONFIG.hn_references.top_stories).limitToFirst(CONFIG.max_num_stories);
 
     return {
         get_stories: function (callback) {
-            firebase.on("value", function (query_snapshot) {
-                query_snapshot.val().forEach(function (value) {
-                    new Firebase(sprintf(CONFIG.hn_references.story, value)).orderByChild('id')
-                        .on("value", function (query_snapshot) {
-                            var item = build_item(query_snapshot.val());
-                            callback(item);
-                        }, function (error) {
-                            console.log("The read failed: " + error.code);
-                        });
-                });
-            }, function (errorObject) {
-                console.log("The read failed: " + errorObject.code);
-            })
+            // Attach an asynchronous callback to read the data at our posts reference
+            // This function will be called anytime new data is added to our Firebase reference, and we don"t need to write any extra code to make this happen.
+            firebase_endpoint.on("value", function (query_snapshot) {
+                    query_snapshot.val().forEach(function (value) {
+                        new Firebase(sprintf(CONFIG.hn_references.story, value)).orderByChild('id')
+                            .on("value", function (query_snapshot) {
+                                var item = build_item(query_snapshot.val());
+                                callback(item);
+                            }, function (error) {
+                                console.log("The read failed: " + error.code);
+                            });
+                    });
+                }, function (errorObject) {
+                    console.log("The read failed: " + errorObject.code);
+                })
         }
     };
 
-    function build_item(source) {
-        if (source !== undefined && source.kids !== undefined && ! source.dead) {
-            var url = source.url !== "" ? source.url : sprintf(CONFIG.hn_story_url, source.id);
-            var comments_url = sprintf(CONFIG.hn_story_url, source.id);
-            var domain = "";
+    // parses and returns an object with the story.
+    function build_item (source) {
+        if (source !== undefined && source.kids !== undefined && !source.dead) {
+            var url = source.url !== "" ? source.url : sprintf(CONFIG.hn_story_url, source.id),
+                comments_url = sprintf(CONFIG.hn_story_url, source.id),
+                domain = "";
             try {
                 domain = new URL(source.url !== "" ? source.url : comments_url).hostname;
                 domain = domain.replace("www.", "");
@@ -51,7 +53,6 @@ app.service("HackerNewsAPI", function () { 'use strict';
                 title: source.title,
                 domain: domain,
                 time: source.time,
-                how_long: Utils.timeAgoFromEpochTime(source.time),
                 score: source.score
             };
         }
