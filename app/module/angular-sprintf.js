b1b9cb7fd13c745bd0c043d40721a37b067ca1b3
Elvis Pestana <elvispt@gmail.com>
2015-03-23 23:50:19 +0000

    Added gulp taks to compress/minify css/js. Now using non minified versions of libraries. Node is required to minify the assets. package.jon sets the depencies required. gulpfile.js defines the tasks in use.

---
+++ app/module/angular-sprintf.js
@@ -0,0 +1,20 @@
+/*! sprintf-js | Alexandru Marasteanu <hello@alexei.ro> (http://alexei.ro/) | BSD-3-Clause */
+
+angular.
+    module("sprintf", []).
+    filter("sprintf", function() {
+        return function() {
+            return sprintf.apply(null, arguments)
+        }
+    }).
+    filter("fmt", ["$filter", function($filter) {
+        return $filter("sprintf")
+    }]).
+    filter("vsprintf", function() {
+        return function(format, argv) {
+            return vsprintf(format, argv)
+        }
+    }).
+    filter("vfmt", ["$filter", function($filter) {
+        return $filter("vsprintf")
+    }])
